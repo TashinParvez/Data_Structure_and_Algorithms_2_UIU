c378b072e10af9543a36bbb9885fee7fbe7e1ab7
Tashin Parvez <tashinparvez2002@gmail.com>
2023-10-17 20:22:15 +0600

    Create DSA-2 Topic Folder

---
+++ DSA_Topics/Greedy ALGO/RPS04.cpp
@@ -0,0 +1,28 @@
+//        ****************  Author :  Tashin.Parvez  *************************\
+//        ****************  Updated:    22-06-23     *************************\
+
+#include <bits/stdc++.h>
+#define CRACKED return 0;
+#define nl endl; // NewLine
+using namespace std;
+
+int SumOfDigits(int n)
+{
+    if (n / 10 == 0)
+        return n;
+    else
+    {
+        return (n % 10) + SumOfDigits(n / 10);
+    }
+}
+
+int32_t main()
+{
+    int n;
+    cin >> n;
+
+    int ans = SumOfDigits(n);
+    cout << "SumOfDigits =  " << ans << nl;
+
+    CRACKED;
+}
