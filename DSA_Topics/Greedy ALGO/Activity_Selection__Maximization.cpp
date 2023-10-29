fbe0f853e828694d844d8311fcf85b8bddda7ccd
Tashin Parvez <tashinparvez2002@gmail.com>
2023-10-29 16:39:29 +0600

    learn Greedy Algo

---
+++ DSA_Topics/Greedy ALGO/Activity_Selection__Maximization.cpp
@@ -0,0 +1,64 @@
+
+//        ****************  Author :  Tashin.Parvez  ****************
+//        ************* United International University *************
+//        ****************  Updated:    27/10/23     ****************
+
+#include <bits/stdc++.h>
+#define CRACKED return 0;
+#define nl endl // NewLine
+#define null NULL
+#define sp " " // space
+#define setDec(x) fixed << setprecision(x)
+#define len(s) s.length()
+#define deb(n) cout << "Tashin   " << #n << " = " << n << endl; // debug code
+
+using namespace std;
+
+bool comperator(vector<int> &v1, vector<int> &v2)
+{
+    if (v1[1] < v2[1])
+        return true;
+    else
+        return false;
+
+    // optimized
+    // return v1[1]<=v2[1];
+}
+
+int32_t main()
+{
+    cout << "Enter total tasks: " << nl;
+    int n;
+    cin >> n;
+    vector<vector<int>> vii;
+
+    cout << "Enter n tasks start and end times: " << nl;
+    for (int i = 0; i < n; i++)
+    {
+        int start, end;
+        cin >> start >> end;
+        vii.push_back({start, end});
+    }
+
+    sort(vii.begin(), vii.end(), comperator); // ascending order based on end time
+
+    // lamda function [custome comperator]
+    // sort(vii.begin(), vii.end(), [&](vector<int> a, vector<int> b)
+    //      { return a[1] < b[1]; });
+
+    int taskTaken = 1;
+    int endTime = vii[0][1];
+    for (int i = 1; i < n; i++)
+    {
+        if (vii[i][0] >= endTime)
+        {
+            taskTaken++;
+            endTime = vii[i][1];
+        }
+    }
+
+    cout << "Task Taken " << taskTaken << nl;
+
+     
+    CRACKED;
+}
---
+++ DSA_Topics/Greedy ALGO/Knapsack_Fractional.cpp
@@ -0,0 +1,79 @@
+
+//        ****************  Author :  Tashin.Parvez  ****************
+//        ************* United International University *************
+//        ****************  Updated:    27/10/23     ****************
+
+#include <bits/stdc++.h>
+#define CRACKED return 0;
+#define nl endl // NewLine
+#define null NULL
+#define sp " " // space
+#define setDec(x) fixed << setprecision(x)
+#define len(s) s.length()
+#define deb(n) cout << "Tashin   " << #n << " = " << n << endl; // debug code
+
+using namespace std;
+
+bool compare(pair<int, int> a, pair<int, int> b)
+{
+    double v1 = (double)a.first / a.second; // value/weight
+    double v2 = (double)b.first / b.second; // value/weight
+
+    return v1 > v2;
+}
+
+int32_t main()
+{
+    cout << "Enter num of products: ";
+    int numOfProducts;
+    cin >> numOfProducts;
+
+    vector<pair<int, int>> vii; // value weight
+
+    cout << "Enter products value and weight: ";
+
+    for (int i = 0; i < numOfProducts; i++)
+    {
+        int a, b;
+        cin >> a >> b; // value weight
+        vii.push_back({a, b});
+    }
+
+    sort(vii.begin(), vii.end(), compare);
+
+    // for sort check
+    // for (int i = 0; i < numOfProducts; i++)
+    // {
+    //     cout << vii[i].first << "  " << vii[i].second << nl;
+    // }
+    // cout << nl;
+
+    cout << "Enter bag size: ";
+    int weight;
+    cin >> weight; // bag size
+
+    int ans = 0; // value in bag
+
+    for (int i = 0; i < numOfProducts; i++)
+    {
+        if (vii[i].second <= weight)
+        {
+            ans += vii[i].first;
+            weight -= vii[i].second;
+            continue;
+        }
+        else
+        {
+            double value = (double)vii[i].first / vii[i].second;
+            ans += value * weight;
+            weight = 0;
+            break;
+        }
+    }
+
+    cout << nl << nl;
+    cout << "Space remain: " << weight << nl;
+    cout << "value of the bag: " << ans << nl;
+
+    CRACKED;
+}
